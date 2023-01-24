a71f1cc192ed9cac5bd83f978915a410ede1eac5
Ignacio-Perez-2023 <ignacio.lopez@kellogg.northwestern.edu>
2023-01-24 20:18:16 +0000

    First code (class 4)

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,46 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
 # Perform simple math with numbers
 
-# Strings
+#puts 5
+#puts 2
+#puts 5 + 2
+#puts 5 * 2
+#puts 5 - 2
+#puts 5 / 2
+# note this gives you the integer only
+#puts 5.0 / 2.0
+# adding decimals makes it give you decimals
+#puts 5 * 2 + 1
 
+
+# Strings (strings are the text in "")
 # Combine strings together
+puts "Hello, world!"
+puts "Tacos are " + "delish"
+#it's called concatenation
+puts "tacos" * 3
+puts "Tacos: " + 3.to_s
 
 # Variables
+food = "Tacos"
+quantity = 3
+puts food * quantity
+# always call variables something that makes sense, not "a" or "b"
+
 
 # Combine strings and variables
+first_name = "Grogu"
+puts "Hello, "+ first_name + "!"
+puts "Hello, #{first_name}!"
+#this is called string interpolation
+greeting = "Hello, #{first_name}!"
+puts greeting
+
+puts "#{food}: #{quantity}"
 
 # String manipulation
+puts first_name.upcase
+puts greeting.reverse
+puts greeting.swapcase
